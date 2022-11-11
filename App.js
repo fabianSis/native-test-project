4186b46950eb368ffc93d6ae0dfb0543512176fa
fabianSis <fabian@asistensi.com>
2022-11-11 10:54:19 -0400

    init

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import {NativeBaseProvider, Box} from 'native-base';
+import {ThemeProvider, Button} from '@asistensi/mich-design';
+
+export default function App() {
+  return (
+    <ThemeProvider>
+      <Button colorScheme='tertiary' >Hello worl a a a a ad</Button>
+    </ThemeProvider>
+  );
+}
